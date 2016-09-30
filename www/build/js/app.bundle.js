b068c6082212b05bf995febeac2af58972deee1c
RacingTomcat <alexander.stier@cutiundstier.de>
2016-09-30 16:43:40 +0200

    - Release 1.1.4

--- www/build/js/app.bundle.js
+++ www/build/js/app.bundle.js
@@ -167,6 +167,29 @@ var FacebookPage = (function () {
         // with MENU-Button
         //this.navController.setRoot(FacebookPage);
     };
+    FacebookPage.prototype.convertISO8601toDate = function (dtstr) {
+        // replace anything but numbers by spaces
+        dtstr = dtstr.replace(/\D/g, " ");
+        // trim any hanging white space
+        dtstr = dtstr.replace(/\s+$/, "");
+        // split on space
+        var dtcomps = dtstr.split(" ");
+        // not all ISO 8601 dates can convert, as is
+        // unless month and date specified, invalid
+        if (dtcomps.length < 3)
+            return "invalid date";
+        // if time not provided, set to zero
+        if (dtcomps.length < 4) {
+            dtcomps[3] = 0;
+            dtcomps[4] = 0;
+            dtcomps[5] = 0;
+        }
+        // modify month between 1 based ISO 8601 and zero based Date
+        dtcomps[1]--;
+        var convdt = new Date(Date.UTC(dtcomps[0], dtcomps[1], dtcomps[2], dtcomps[3], dtcomps[4], dtcomps[5]));
+        return convdt.toLocaleString();
+        ;
+    };
     FacebookPage = __decorate([
         core_1.Component({
             templateUrl: 'build/pages/facebook/facebook.html',
